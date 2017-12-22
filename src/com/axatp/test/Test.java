02fd0dfb709c09bd004f514eea46b88fc4cbd01d
yzzy03 <zhangzx3@axatp.com>
2017-12-22 23:40:35 +0800

    Test

---
+++ src/com/axatp/test/Test.java
@@ -0,0 +1,11 @@
+package com.axatp.test;
+
+/**
+ * Created by zhangzhongxia on 2017/12/22.
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.println("hello world!");
+    }
+
+}
